9aa20ae55be42f1dec5c6343bdd98d8fa8e5caf9
Ramakant Mishra <ramakantm921@gmail.com>
2026-06-24 23:56:10 +0530

    something

---
+++ basics/dataType.cpp
@@ -0,0 +1,17 @@
+// Basics datatype
+
+
+#include<iostream>
+using namespace std;
+
+int main (){
+    int myNum = 5;
+    float myFloatNum = 5.99;
+    double myDoubleNum = 9.98;
+    char myLetter = 'D';
+    bool myBoolean = true;
+    string myText = "Hello";
+    cout << myNum << "\n";
+    cout << myBoolean << "\n";
+    cout << myText;
+}
---
+++ basics/first.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // cout << "Hello world!";
+    // std::cout << "Hello World!";
+    // return 0;
+
+    cout << "Hello World!" << "\n";
+    cout << "Hello World!" << "\n\n";
+    cout << "I am learning c++";
+}
+
